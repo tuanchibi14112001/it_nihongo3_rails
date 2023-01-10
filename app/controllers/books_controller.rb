fb5dcfd44b31c225ff5e2dfe1a26278003be4eb9
tuanchibi <tuanchibi14112001@gmail.com>
2023-01-10 10:14:08 +0000

    Transition to Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /books or /books.json
   def index
     @books = Book.all
